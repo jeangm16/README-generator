86a4c09537bf6d6a5908e525351724d53dcb4c3a
John Melendez <john@Johns-MBP.domain>
2020-10-14 15:16:47 -0600

    first commit added declaring variables

---
+++ index.js
@@ -0,0 +1,6 @@
+// Declaring the dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadme = require("./utils/generateReadme")
+const writeFileAsync = util.promisify(fs.writeFile);
